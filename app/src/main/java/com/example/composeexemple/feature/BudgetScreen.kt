c42f9985680e49965c2531584e300abb2be2c6d0
Lucas Silva <lucasvida2005@gmail.com>
2024-08-07 16:48:41 -0300

    Reducing the code and finishing some things

--- app/src/main/java/com/example/composeexemple/feature/BudgetScreen.kt
+++ app/src/main/java/com/example/composeexemple/feature/BudgetScreen.kt
@@ -1,7 +1,6 @@
 package com.example.composeexemple.feature
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,18 +8,21 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -41,15 +43,25 @@ fun MainText(modifier: Modifier = Modifier) {
     )
 }
 
-// Componente para entrada de ganhos
+// Componente para entrada de ganhos e gastos
 @Composable
-fun BudgetArea(modifier: Modifier = Modifier) {
+fun BudgetArea(
+    modifier: Modifier = Modifier,
+    title: String
+) {
     val (value, setValue) = remember { mutableStateOf("") }
     val (selectedOption, setSelectedOption) = remember { mutableStateOf("") }
+    val savedValues = remember { mutableStateListOf<String>() }
+
+    val options = if (title == "GANHOS") {
+        listOf("Emprego", "Investimentos", "Freelance", "Aluguel", "Pensão", "Prêmios", "Aposentadoria")
+    } else {
+        listOf("Casa", "Alimentação", "Transporte", "Educação", "Saúde", "Lazer", "Roupas")
+    }
 
     Column(modifier = modifier) {
         Text(
-            text = "GANHOS",
+            text = title,
             modifier = Modifier.padding(horizontal = 40.dp),
             fontWeight = FontWeight.Bold
         )
@@ -58,7 +70,7 @@ fun BudgetArea(modifier: Modifier = Modifier) {
             modifier = Modifier.padding(horizontal = 40.dp)
         ) {
             ExposedDropMenu(
-                options = List(10) { "Opção ${it + 1}" },
+                options = options,
                 selectedOption = selectedOption,
                 onOptionSelected = setSelectedOption
             )
@@ -72,7 +84,13 @@ fun BudgetArea(modifier: Modifier = Modifier) {
         }
         Spacer(modifier = Modifier.height(10.dp))
         Button(
-            onClick = { /*TODO*/ },
+            onClick = {
+                if (selectedOption.isEmpty() || value.isEmpty()) return@Button
+                savedValues.add("$selectedOption | R$ $value")
+
+                setSelectedOption("")
+                setValue("")
+            },
             shape = RoundedCornerShape(5.dp),
             modifier = Modifier
                 .fillMaxWidth()
@@ -84,74 +102,37 @@ fun BudgetArea(modifier: Modifier = Modifier) {
                 contentDescription = "Adicionar"
             )
         }
-        Spacer(modifier = Modifier.height(10.dp))
-        Box(
+        Spacer(modifier = Modifier.height(5.dp))
+        Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(140.dp)
+                .height(130.dp)
                 .padding(horizontal = 40.dp)
                 .background(Seashell)
-                .padding(16.dp)
+                .verticalScroll(rememberScrollState())
+                .weight(weight = 1f, fill = false)
         ) {
-            // TODO: Adicionar funcionamento para adicionar os items
+            savedValues.forEach {
+                Text(it)
+            }
         }
     }
 }
 
-// Componente para a entrada de gastos
+// Componente para o botao de continuar
 @Composable
-fun SpendingArea(modifier: Modifier = Modifier) {
-    val (spendingValue, setSpendingValue) = remember { mutableStateOf("") }
-    val (selectedSpendingOption, setSelectedSpendingOption) = remember { mutableStateOf("") }
-
-    Column(modifier = modifier) {
-        Text(
-            text = "GASTOS",
-            modifier = Modifier.padding(horizontal = 40.dp),
-            fontWeight = FontWeight.Bold
+fun StageButton(modifier: Modifier = Modifier) {
+    Button(
+        onClick = { /*TODO*/ },
+        shape = RoundedCornerShape(5.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 40.dp)
+    ) {
+        Icon(
+            imageVector = Icons.Default.Check,
+            contentDescription = "Finalizar"
         )
-        Spacer(modifier = Modifier.height(15.dp))
-        Row(
-            modifier = Modifier.padding(horizontal = 40.dp)
-        ) {
-            ExposedDropMenu(
-                options = List(10) { "Opção ${it + 1}" },
-                selectedOption = selectedSpendingOption,
-                onOptionSelected = setSelectedSpendingOption
-            )
-            Spacer(modifier = Modifier.width(5.dp))
-            OutlinedTextField(
-                value = spendingValue,
-                onValueChange = setSpendingValue,
-                prefix = { Text(text = "R$") },
-                maxLines = 1
-            )
-        }
-        Spacer(modifier = Modifier.height(10.dp))
-        Button(
-            onClick = { /*TODO*/ },
-            shape = RoundedCornerShape(5.dp),
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(40.dp)
-                .padding(horizontal = 40.dp)
-        ) {
-            Icon(
-                imageVector = Icons.Default.Add,
-                contentDescription = "Adicionar"
-            )
-        }
-        Spacer(modifier = Modifier.height(10.dp))
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(140.dp)
-                .padding(horizontal = 40.dp)
-                .background(Seashell)
-                .padding(16.dp)
-        ) {
-            // TODO: Adicionar funcionamento para adicionar os items
-        }
     }
 }
 
@@ -161,9 +142,11 @@ fun BudgetScreen(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(vertical = 16.dp)) {
         MainText()
         Spacer(modifier = Modifier.height(30.dp))
-        BudgetArea()
+        BudgetArea(title = "GANHOS")
         Spacer(modifier = Modifier.height(20.dp))
-        SpendingArea()
+        BudgetArea(title = "GASTOS")
+        Spacer(modifier = Modifier.weight(1f))
+        StageButton()
     }
 }
 
